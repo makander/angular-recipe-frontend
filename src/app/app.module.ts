08e5503d3d3afefda82b1163eac6993425e3ca03
makander@gmail.com <nils@DESKTOP-N57VP0D.localdomain>
2019-02-17 20:04:21 +0100

    change structure, remove recipe item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailsComponent } from './recipes/recipe-list/recipe-details/recipe-details.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClient } from 'selenium-webdriver/http';
 
@@ -16,7 +15,6 @@ import { HttpClient } from 'selenium-webdriver/http';
     AppComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeItemComponent,
     RecipeDetailsComponent,
     HeaderComponent
   ],
