c9db6cfcc11838afca918c5b45c7e5989145f3a3
Nils Makander <makander@gmail.com>
2019-02-21 17:51:25 +0100

    add some styling and links for register and sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.component";
 import { FavoritesComponent } from "./recipes/favorites/favorites.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   { path: "recipes", component: RecipeListComponent },
   { path: "recipes/:id", component: RecipeDetailsComponent },
-  { path: "favorites", component: FavoritesComponent }
+  { path: "favorites", component: FavoritesComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "profile", component: ProfileComponent }
   //{ path: '', component: RecipeListComponent }
 ];
 
